cf5a02649432aae455792a42890d727d5117780a
Arthus Benazio <arthusbenazio@gmail.com>
2023-12-10 15:57:59 -0300

    First Commit

---
+++ app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
@@ -0,0 +1,95 @@
+package com.devmasterteam.tasks.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import com.devmasterteam.tasks.R
+import com.devmasterteam.tasks.databinding.ActivityLoginBinding
+import com.devmasterteam.tasks.viewmodel.LoginViewModel
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Variáveis da classe
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        // Layout
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        // Eventos
+        binding.buttonLogin.setOnClickListener(this)
+        binding.textRegister.setOnClickListener(this)
+
+        viewModel.verifyAuthetication()
+
+        // Observadores
+        observe()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_login) {
+            handleLogin()
+        } else if (v.id == R.id.text_register) {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    private fun observe() {
+        viewModel.login.observe(this) {
+            if (it.status()) {
+                startActivity((Intent(applicationContext, MainActivity::class.java)))
+                finish()
+            } else {
+                Toast.makeText(applicationContext, it.message(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.loggedUser.observe(this) {
+            if (it) {
+
+                biometricAuthenticate()
+
+            }
+        }
+
+    }
+
+    private fun biometricAuthenticate() {
+        val executer = ContextCompat.getMainExecutor(this)
+        val bio = BiometricPrompt(this, executer, object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                startActivity(Intent(applicationContext,MainActivity::class.java))
+                finish()
+            }
+        })
+
+        val info = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Title")
+            .setSubtitle("Subtitle")
+            .setDescription("description")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        bio.authenticate(info)
+    }
+
+    private fun handleLogin() {
+        val email = binding.editEmail.text.toString()
+        val password = binding.editPassword.text.toString()
+
+        viewModel.doLogin(email, password)
+    }
+}
